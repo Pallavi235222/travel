5eb2418caca1f998a6dde6baa887b460831a9b87
Pallavi <pallavis235222@gmail.com>
2022-12-17 21:13:34 +0530

    initial commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,66 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image,TouchableOpacity } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import * as Animatable from 'react-native-animatable';
+import { useNavigation } from '@react-navigation/native'
+import { HeroImage } from '../assets';
+
+
 
 const HomeScreen = () => {
+
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    })
+  },
+    []
+  )
   return (
-    <View>
-      <Text className="text-red-400">HomeScreen</Text>
-    </View>
+    <SafeAreaView className="bg-white flex-1 relative">
+      {/* first section */}
+
+      <View className="flex-row px-6 mt-8 items-center space-x-2">
+        <View className="w-16 h-16 bg-black rounded-full items-center justify-center">
+          <Text className="text-[#00BCC9] text-3xl font">Go</Text>
+        </View>
+
+        <Text className="text-[#2A2B4B] text-3xl font-semibold">Travel</Text>
+      </View>
+
+      {/* second section */}
+      <View className="px-6 mt-8 space-y-3">
+        <Text className="text-[#3C6072] text-[42px] " >Enjoy the trip with</Text>
+        <Text className="text-[#00BCC9] text-[38px] font-bold">Good Moments</Text>
+
+        <Text className="text-[#3C6072] text-base">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        </Text>
+      </View>
+
+      {/* circle section */}
+      <View className="w-[400px] h-[400px] bg-[#00BCC9] rounded-full absolute bottom-36 -right-36"></View>
+      <View className="w-[400px] h-[400px] bg-[#E99265] rounded-full absolute -bottom-28 -left-36"></View>
+
+      {/* Image container */}
+      <View className="flex-1 relative items-center justify-center">
+        <Animatable.Image
+        // animation="fadeIn"
+        // easing="ease-in-out"
+          source={HeroImage}
+          className="w-full h-full object-cover mt-20"
+        />
+
+        <View className="absolute bottom-20 w-24 h-24 border-l-2 border-r-2 border-t-4 border-[#008CC9] rounded-full items-center justify-start">
+          <TouchableOpacity>
+            <View className="w-20 h-20 items-center justify-center rounded-full bg-[#008CC9]">
+              <Text className="text-gray-50 text-[36px] font-semibold">Go</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
